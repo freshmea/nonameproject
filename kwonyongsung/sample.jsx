c6b168217222db120784aad0a10d5777cd2c1434
kwonyongsung <dydtjd7064@naver.com>
2022-10-07 12:02:57 +0900

    a

--- kwonyongsung/sample.jsx
+++ kwonyongsung/sample.jsx
@@ -8,12 +8,8 @@ import CarStop from "../CarStop.js";
 function App(){
 	let [picture, pictureChange] = useState(CarGo);
 	
-	
 	return <div className="App">
 		{picture}
-		
-		<!-- 데이터 받기 -> pictureChange(CarStop) -->
-		
 		<button onClick={ ()=>{ pictureChange(CarGo) } }>Go</button> &nbsp;
 		<button onClick={ ()=>{ pictureChange(CarWait) } }>Wait</button> &nbsp;
 		<button onClick={ ()=>{ pictureChange(CarStop) } }>Stop</button>
