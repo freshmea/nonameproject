ba90b1604417d52877ba5aba4d463fb292cde08f
kwonyongsung <dydtjd7064@naver.com>
2022-10-11 15:50:52 +0900

    a

--- kwonyongsung/net communication/CarStop.js
+++ kwonyongsung/net communication/CarStop.js
@@ -1,9 +1,9 @@
 var net = require('net');
 
-var client = net.connect({port: 59792, host:'43.200.117.188'},function(){
+var client = net.connect({port:58409, host:'13.125.173.103'},function(){
   //net모듈의 소켓 객체를 사용    
   console.log('Client connected');    
-  client.write('Some Data\r\n');
+  client.write('CarStop\r\n');
 });
 
 client.on('data',function(data){
