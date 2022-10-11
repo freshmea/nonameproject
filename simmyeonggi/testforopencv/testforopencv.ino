a2e276b7488b719c27b0d46b5e17eaf220198b14
qwe <qwe>
2022-10-11 16:44:48 +0900

    asd

--- simmyeonggi/testforopencv/testforopencv.ino
+++ simmyeonggi/testforopencv/testforopencv.ino
@@ -4,22 +4,42 @@ const int carR = 4;
 
 const int pedR = 5;
 const int pedG = 6;
+bool signal = false;
+char buf[50];
 
-
-void carGo()
+long time = millis() ;
+void carGo(bool signal)
 {
-  digitalWrite(carG, HIGH);
-  digitalWrite(pedR, HIGH);
-  digitalWrite(carY, LOW);
-  digitalWrite(carR, LOW);
-  digitalWrite(pedG, LOW);  
+  if (signal){
+    digitalWrite(carG, HIGH);
+    digitalWrite(pedR, HIGH);
+    digitalWrite(carY, LOW);
+    digitalWrite(carR, LOW);
+    digitalWrite(pedG, LOW); 
+  }else{
+    digitalWrite(carG, LOW);
+    digitalWrite(carY, LOW);
+    digitalWrite(carR, HIGH);
+    digitalWrite(pedR, LOW);
+    digitalWrite(pedG, HIGH); 
+  }
 }
 
-void carWait()
+void carWait(bool signal)
 {
-  digitalWrite(carG, LOW);
-  digitalWrite(carY, HIGH);
-  digitalWrite(carR, LOW);
+  if (signal){
+    digitalWrite(carG, LOW);
+    digitalWrite(carY, HIGH);
+    digitalWrite(carR, LOW);
+    digitalWrite(pedR, HIGH);
+    digitalWrite(pedG, LOW);
+  }else{
+    digitalWrite(carG, LOW);
+    digitalWrite(carY, LOW);
+    digitalWrite(carR, HIGH);
+    digitalWrite(pedR, LOW);
+    digitalWrite(pedG, HIGH); 
+  }
 }
 
 void carStop()
@@ -39,18 +59,51 @@ void setup() {
   pinMode(pedR, OUTPUT);
   pinMode(pedG, OUTPUT);
 
-  carGo();
+  carWait(signal);
+  
+  time = millis() ; 
 }
 
 void loop() {
-  if (Serial.available() > 0){
-    if (Serial.read() >= 49){ 
-      Serial.println("신호등 바꾸기");
-      carWait();
-      delay(3000);
-      carStop();
-      delay(10000);
-      carGo();
+  int cnt = 0;
+  while (Serial.available() > 0){
+    char t = Serial.read();
+    if (t == '\n'){
+      break;
+    }
+    if (t == '1'){
+      Serial.println(Serial.read());
+      signal = true;
+    }
+    if (t == '0'){
+      signal = false;
     }
   }
+
+  
+    
+  if (time + 3000 > millis()) {
+    carWait(signal);
+  }else if (time + 6000 > millis()) {
+    carStop();
+  }else if (time + 9000 > millis()){
+    carGo(signal);
+     
+  }else{
+    time = millis() ;
+  }
 }
+//
+//  if (Serial.available() > 0){
+//    if (Serial.read() >= 49){
+//      time = millis() ; 
+//      carWait();
+//    }
+//  
+//  if (time + 3000 < millis()) {
+//    carStop();
+//    }
+//  if ((time+3000> millis()) &&(time + 13000 < millis())0 {
+//    carGo();
+//    time = millis() ;
+//    }
