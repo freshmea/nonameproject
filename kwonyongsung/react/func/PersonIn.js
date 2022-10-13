b77d61a74db09788df91afab3e5a971d7185d227
kwonyongsung <dydtjd7064@naver.com>
2022-10-13 16:34:21 +0900

    10/13

---
+++ kwonyongsung/react/func/PersonIn.js
@@ -0,0 +1,10 @@
+
+function PersonIn(){
+	return(
+		<div className="Person">
+			<img src="image/person1.png" alt = ""/>
+		</div>
+	)
+}
+
+export default PersonIn;
---
+++ kwonyongsung/react/func/PersonOut.js
@@ -0,0 +1,10 @@
+
+function PersonOut(){
+	return(
+		<div className="Person">
+			<img src="image/person2.png" alt = ""/>
+		</div>
+	)
+}
+
+export default PersonOut;
