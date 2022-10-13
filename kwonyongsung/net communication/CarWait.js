df78c9b089b3c28e1798d34caa452b4f96096fa3
kwonyongsung <dydtjd7064@naver.com>
2022-10-13 09:07:42 +0900

    a

--- kwonyongsung/net communication/CarWait.js
+++ kwonyongsung/net communication/CarWait.js
@@ -1,6 +1,6 @@
 var net = require('net');
 
-var client = net.connect({port:58409, host:'13.125.173.103'},function(){
+var client = net.connect({port:51343, host:'13.125.173.103'},function(){
   //net모듈의 소켓 객체를 사용    
   console.log('Client connected');    
   client.write('CarWait\r\n');
